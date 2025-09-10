4a427eb8fd9fd5bb37d91555996c7c19fb51efbc
fredsoto641-spec <fredsoto641@gmail.com>
2025-09-09 20:05:43 -0500

    change : carpeta notas

--- assets/notas/notas.java
+++ assets/notas/notas.java
@@ -1,2 +1,3 @@
 //Se quiere implemento un registro exitoso en la pagina web mediante un host de xampp
 //Se creo un usuario y contraseña exitosamente
+//Se creo una base de datos guia_turistica mediante xampp
