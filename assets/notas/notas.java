6d43df4924447cf8fd5afe96eac1bf89b823bab2
fredsoto641-spec <fredsoto641@gmail.com>
2025-09-13 12:13:38 -0500

    fix: usuario admin responde ahora correctamente

--- assets/notas/notas.java
+++ assets/notas/notas.java
@@ -2,3 +2,4 @@
 //Se creo un usuario y contraseña exitosamente
 //Se creo una base de datos guia_turistica mediante xampp
 //Se creo una base con guia_turistica
+//La pagina responde correctamente al usuario admin
