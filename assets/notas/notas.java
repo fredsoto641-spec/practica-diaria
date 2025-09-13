d2a4bf08670a30b2f27483e1c1a098f33239332b
fredsoto641-spec <fredsoto641@gmail.com>
2025-09-13 12:12:29 -0500

    update : notas

--- assets/notas/notas.java
+++ assets/notas/notas.java
@@ -1,3 +1,4 @@
 //Se quiere implemento un registro exitoso en la pagina web mediante un host de xampp
 //Se creo un usuario y contraseña exitosamente
 //Se creo una base de datos guia_turistica mediante xampp
+//Se creo una base con guia_turistica
